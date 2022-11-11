1a7425c6cc7e1b0cc5dff389c8e3f06ca3ce02bb
Xiaojing <8redfishcreative@gmail.com>
2022-11-11 14:05:54 -0500

    finish tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -11,10 +11,6 @@ try{
     include: [{
         model: Product,
         as: 'products',
-        include: [{
-            model: ProductTag,
-            as: 'product_tag'
-       }]
     }]
 });
     res.status(200).json(tagData);
@@ -23,22 +19,62 @@ try{
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-
+ try{
+    const tagData = await Tag.findByPk(req.params.id,{
+    include: [{model: Product, as: 'products'}]
+  });
+     res.status(200).json(tagData);
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+ try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+ try{
+    const tag = await Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+    },
+    });
+    res.status(200).json(tag);
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+try{
+    const tagDate = await Tag.destroy({
+    where: {
+       id: req.params.id
+    }
+    })
+    if(!tagDate){
+       res.status(404).json({message: 'No tag found with this id!'})
+       return;
+    }
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
